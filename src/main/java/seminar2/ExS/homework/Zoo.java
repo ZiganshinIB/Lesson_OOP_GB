6cfd8946173e8f4813574fc83b973c9772c2a8b4
ZiganshinIB <elzig3012@gmail.com>
2023-04-16 20:01:49 +0300

    Create Seminar 2: Zoo park

---
+++ src/main/java/seminar2/ExS/homework/Zoo.java
@@ -0,0 +1,55 @@
+package seminar2.ExS.homework;
+
+import seminar2.ExS.homework.base.Animal;
+import seminar2.ExS.homework.base.ICollection;
+import seminar2.ExS.homework.base.IVoice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zoo implements ICollection<Animal>, IVoice {
+    private List<Animal> animals;
+    public Zoo() {
+        animals = new ArrayList<>();
+    }
+
+    public Zoo(List<Animal> animals) {
+        this.animals = animals;
+    }
+
+    @Override
+    public void add(Animal element) {
+        animals.add(element);
+    }
+
+    @Override
+    public boolean remote(Animal element) {
+        return animals.remove(element);
+    }
+
+    @Override
+    public Animal get(int index) {
+        var result = animals.get(index);
+        System.out.println(result);
+        return result;
+    }
+
+    @Override
+    public Animal remote(int index) {
+        return animals.remove(index);
+    }
+
+    @Override
+    public void infoAll() {
+        for (var animal: animals) {
+            System.out.println(animal);
+        }
+    }
+
+    @Override
+    public void getVoice() {
+        for(var animal: animals){
+            animal.getVoice();
+        }
+    }
+}
